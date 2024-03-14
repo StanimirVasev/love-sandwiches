7818d8ecddc5d84a6553cb42924a615713dd4da7
StanimirVasev <s.vasev97@gmail.com>
2024-03-14 23:57:59 +0000

    Finalised the Code Institute challenge

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
+# Every Google account has as an IAM (Identity and Access Management)
+# configuration which specifies what the user has access to.
+# The SCOPE lists the APIs that the program should access in order to run.
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -14,18 +18,20 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
+
 def get_sales_data():
     """
     Get sales figures input from the user.
-    Run a while loop to collect a valid string of data from the user
+    We run a while loop to collect a valid string of data from the user
     via the terminal, which must be a string of 6 numbers separated
     by commas. The loop will repeatedly request data, until it is valid.
     """
+
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
-
+        
         data_str = input("Enter your data here: ")
 
         sales_data = data_str.split(",")
@@ -47,8 +53,8 @@ def validate_data(values):
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
-            )
+                f"Exactly 6 values are required, you provided {len(values)}")
+
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
@@ -56,15 +62,36 @@ def validate_data(values):
     return True
 
 
+# def update_sales_worksheet(data):
+#     """
+#     Update sales worksheet, add new row with the list data provided.
+#     """
+#     print("Updating sales worksheet...\n")
+#     sales_worksheet = SHEET.worksheet("sales")
+#     sales_worksheet.append_row(data)
+#     print("Sales worksheet updated succesfully.\n")
+
+
+# def update_surplus_worksheet(data):
+#     """
+#     Update surplus worksheet, add new row with the list data provided.
+#     """
+#     print("Updating surplus worksheet...\n")
+#     surplus_worksheet = SHEET.worksheet("surplus")
+#     surplus_worksheet.append_row(data)
+#     print("Surplus worksheet updated succesfully.\n")
+
+
 def update_worksheet(data, worksheet):
     """
-    Receives a list of integers to be inserted into a worksheet
-    Update the relevant worksheet with the data provided
+    Receives a list of integers to be inserted into a worksheet.
+    Update the relevant worksheet, add new row with the list data provided.   
     """
-    print(f"Updating {worksheet} worksheet...\n")
+    print(f"Updating {worksheet} worksheet...\n") 
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully\n")
+    print(f"{worksheet} worksheet updated succesfully.\n")
+
 
 
 def calculate_surplus_data(sales_row):
@@ -72,14 +99,15 @@ def calculate_surplus_data(sales_row):
     Compare sales with stock and calculate the surplus for each item type.
 
     The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplus indicates waste
+    - Positive surplus indicates waste.
     - Negative surplus indicates extra made when stock was sold out.
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    
+
     surplus_data = []
+
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
@@ -87,12 +115,13 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def get_last_5_entries_sales():
+def get_last_5_entires_sales():
     """
-    Collects columns of data from sales worksheet, collecting
-    the last 5 entries for each sandwich and returns the data
+    Collect columns of data from sales worksheet.
+    Get the last 5 entries for each sandwich and return the data
     as a list of lists.
     """
+
     sales = SHEET.worksheet("sales")
 
     columns = []
@@ -105,33 +134,55 @@ def get_last_5_entries_sales():
 
 def calculate_stock_data(data):
     """
-    Calculate the average stock for each item type, adding 10%
+    Calculate the average stock for each item type, adding 10%.
     """
     print("Calculating stock data...\n")
     new_stock_data = []
 
     for column in data:
         int_column = [int(num) for num in column]
+
         average = sum(int_column) / len(int_column)
         stock_num = average * 1.1
         new_stock_data.append(round(stock_num))
 
     return new_stock_data
 
-
 def main():
     """
-    Run all program functions
+    Run all program functions.
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
-    new_surplus_data = calculate_surplus_data(sales_data)
-    update_worksheet(new_surplus_data, "surplus")
-    sales_columns = get_last_5_entries_sales()
+
+    new_surplus_row = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_row, "surplus")
+
+    sales_columns = get_last_5_entires_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
-print("Welcome to Love Sandwiches Data Automation")
-main()
+print("Welcome to Love Sandwiches data automation.\n")
+stock_data = main()
+
+# Write you code below this comment
+
+def get_stock_values(data):
+    """
+    Getting a dictionary to advise on number of sandwiches for next market 
+    """
+    
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+    
+    dictionary = {}
+    for heading, num in zip(headings, data):
+        dictionary[heading] = num
+    
+    return dictionary
+    
+stock_values = get_stock_values(stock_data)
+print("Make the following number of sandwiches for next market:\n")
+print(stock_values)
